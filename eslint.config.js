f3c75e7696263ba95a7b111ba3c16f5f3ac85256
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-11-09 14:05:38 +0300

    removed sonarjs

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
 import js from "@eslint/js";
 import oxlint from "eslint-plugin-oxlint";
-import sonarjs from "eslint-plugin-sonarjs";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -10,16 +9,10 @@ export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
-  sonarjs.configs.recommended,
   {
     rules: {
       "@typescript-eslint/no-explicit-any": 0,
       "@typescript-eslint/consistent-type-definitions": 0,
-      "sonarjs/todo-tag": "warn",
-      "sonarjs/new-cap": 0,
-      "sonarjs/no-commented-code": 0,
-      "sonarjs/no-identical-functions": 0,
-      "sonarjs/public-static-readonly": 0,
     },
     languageOptions: {
       ecmaVersion: "latest",
