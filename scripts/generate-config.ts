bd92ea8385930ff8f47e084f70cdb5af4086e206
Toil <62353659+ilyhalight@users.noreply.github.com>
2026-07-20 23:23:47 +0300

    chore: bump depends and last changes for 1.1.0

--- scripts/generate-config.ts
+++ scripts/generate-config.ts
@@ -1,4 +1,5 @@
 import path from "node:path";
+import { format } from "oxfmt";
 
 const rootPath = path.resolve(__dirname, "..");
 
@@ -6,13 +7,15 @@ const packageInfoPath = path.resolve(rootPath, "package.json");
 const packageInfoFile = Bun.file(packageInfoPath);
 const packageInfo = (await packageInfoFile.json()) as { version: string };
 
-await Bun.write(
-  path.resolve(rootPath, "src", "config.ts"),
+const CONFIG_PATH = path.resolve(rootPath, "src", "config.ts");
+const code = await format(
+  CONFIG_PATH,
   `// This file is auto-generated. Don't modify it manually
 export default {
   version: "${packageInfo.version}",
   debug: false,
   fetchFn: fetch.bind(window),
-};
-`,
+};`,
 );
+
+await Bun.write(CONFIG_PATH, code.code);
